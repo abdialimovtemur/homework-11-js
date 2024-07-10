52be4febd5d8c37585b026775989c04bff057eb7
abdialimovtemur <abdialimovtemur@gmail.com>
2024-07-10 08:56:25 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// 1. argument sifatida berilgan sonning nechchi xonali ekanligini aniqlaydigan function tuzing
+
+
+// let userNum = prompt("Sonni kiriting");
+// let total = 0;
+
+// function numberofRooms() {
+//     let str = userNum.toString().split("")
+//     for(let i = 0; i < str.length; i++){
+//         total += 1;
+//     }
+//     console.log(total)
+// }
+// numberofRooms()
+
+
+
+// 2. argument sifatida berilgan stringda nechta son qatnashganligini aniqlaydigan function tuzing
+
+// let userStr = prompt("So'zlarni kiriting");
+
+// function seperateNum() {
+
+// }
+
+
+
+
+
+
+// 3. argument sifatida berilgan stringda nechta so’zda a harfi ishtirok etganini aniqlaydigan function tuzing
+
+// let  userStr = prompt("So'zni kiriting")
+
+// function strFilterA(arr) {
+//     let total = 0;
+//     arr.split("")
+//     console.log(arr)
+//     for(let i = 0; i < arr.length; i++){
+//         if (arr[i] == "a") {
+//             total++;
+//         }
+//     }
+//     console.log(total)
+// }
+// strFilterA(userStr)
+
+
+
+// 4. Gapdagi eng uzun va eng qisqa so’zlarni aniqlab {minWord: “…”, maxWord: “….”} qiymat qaytaruvchi minManWord() nomli function yarating Input: “Men dasturlash kursida o’qiyman” Output: {minWord: “Men”, maxWord: “dasturlash”} 
+
+
+
+
+
+// 5. Stringlar arrayi berilgan. Shunday object hosil qilinki o’sha object kalitlari array elementlaridan va qiymatlari ularning uzuligindan iborat bo’lsin Input: [”text”, “world”, “laptop”] Output: {”text”: 4, “world”: 4, “laptop”: 6} 
+
+
+
+
+
+// 6. n natural soni berilgan 2 sonining dastlabki nta darajasidan tashkil topgan arrayni qaytaruvchi getLevel(n) nomli function tuzing n: 4 result : [2,4,8,16] 2 ning 1-darajasidan boshlanishi kerak va 4-darajasigacha 
+
+
+
+// let userNum = prompt("Sonni kiriting");
+// function nToDegree() {
+//     let sum = 1;
+//     let arr = [];
+//     for (let i = 1; i <= userNum; i++) {
+//         sum = sum * 2
+//         arr.push(sum)
+//     }
+//     console.log(arr)
+// }
+
+// nToDegree()
